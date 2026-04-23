878e9b6dd15c166f24375dbdc7c28832a200f60d
Frenson7hotang <129707266+Frenson7hotang@users.noreply.github.com>
2026-04-23 19:15:40 +0700

    add readonly

--- edit.php
+++ edit.php
@@ -16,7 +16,7 @@
 <form class="max-w-sm mx-auto pt-12" method="POST" action="proses/ubah.php">
   <div class="mb-5">
     <label for="id" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">ID</label>
-    <input type="text" name="id" id="id" value="<?php echo $d['id']?>" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required />
+    <input type="text" name="id" id="id" value="<?php echo $d['id']?>" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" readonly />
   </div>
   <div class="mb-5">
     <label for="nama" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nama</label>
